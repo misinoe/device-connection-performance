0f149dae13d687b003af78fe59f9d664901a8328
misinoe <misinoe.t@gmail.com>
2017-06-11 15:35:16 +0900

    modified express

--- express/socket/socket.js
+++ express/socket/socket.js
@@ -1,15 +1,25 @@
 const SocketIo = require('socket.io');
-
 class Socket {
   constructor() {
     const io = this.io = SocketIo();
     io.on('connection', socket => {
       console.log('client connected', socket.id);
-      socket.on('ping', function(data) {
-        data.wrap = new Date().getTime();
-        io.emit('echo', data);
-      });
     });
+    const ns = this.ns = io.of('/ns');
+    ns.on('connection', socket => {
+      socket.on('ppp', data => {
+        data.wrap = this.getNanoSecond();
+        socket.emit('echo', data);
+        console.log('ping => echo');
+      });
+      socket.emit('hello');
+      console.log('ns connected');
+    });
+  }
+
+  getNanoSecond() {
+    let hrtime = process.hrtime();
+    return (hrtime[0] * 1000) + (hrtime[1] / 1000000);
   }
 
   attach(server) {
