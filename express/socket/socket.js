c87a506170fc1e10d7610b8dd9468046a85dc558
misinoe <misinoe.t@gmail.com>
2017-06-15 23:40:15 +0900

    p2p

--- express/socket/socket.js
+++ express/socket/socket.js
@@ -1,20 +1,13 @@
 const SocketIo = require('socket.io');
+const SocketP2p = require('socket.io-p2p-server');
+
 class Socket {
   constructor() {
     const io = this.io = SocketIo();
     io.on('connection', socket => {
       console.log('client connected', socket.id);
     });
-    const ns = this.ns = io.of('/ns');
-    ns.on('connection', socket => {
-      socket.on('ppp', data => {
-        data.wrap = this.getNanoSecond();
-        socket.emit('echo', data);
-        console.log('ping => echo');
-      });
-      socket.emit('hello');
-      console.log('ns connected');
-    });
+    const p2p = this.p2p = io.use(SocketP2p.Server);
   }
 
   getNanoSecond() {
--- src/vue/entries/index.vue
+++ src/vue/entries/index.vue
@@ -2,7 +2,38 @@
 </style>
 <template>
   <div>
-    <mdl-button v-on:click.native="sendPing()">send ping</mdl-button>
+    <p>socket.io-p2p status</p>
+    <ul>
+      <li v-for="status in p2pStatusList">{{status}}</li>
+    </ul>
+    <mdl-button v-on:click.native="repeatPingP2p()">send ping p2p</mdl-button>
+    <div class="mdl-layout">
+      <div class="mdl-grid">
+        <p>count: {{p2pCount}}, AVG: {{fix(p2pTotalDelta / p2pCount)}}</p>
+      </div>
+      <div class="mdl-grid">
+        <table class="mdl-data-table">
+          <thead>
+            <tr>
+              <th>time(ms)</th>
+              <th>now(ms)</th>
+              <th>delta(ms)</th>
+              <th>size</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in p2pEchoList">
+              <td>{{fix(item.time)}}</td>
+              <td>{{fix(item.now)}}</td>
+              <td>{{fix(item.delta)}}</td>
+              <td>{{item.size}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <mdl-button v-on:click.native="repeatPing()">send ping</mdl-button>
     <div class="mdl-layout">
       <div class="mdl-grid">
         <p>count: {{count}}, AVG: {{fix(totalDelta / count)}}</p>
@@ -35,6 +66,7 @@
 import Vue from 'vue';
 import VueMdl from 'vue-mdl';
 import SocketIoClient from 'socket.io-client';
+import SocketP2p from 'socket.io-p2p';
 
 Vue.use(VueMdl);
 
@@ -50,10 +82,14 @@ export default {
       echoList: [],
       totalDelta: 0,
       count: 0,
+      p2pEchoList: [],
+      p2pTotalDelta: 0,
+      p2pCount: 0,
+      p2pStatusList: [],
     };
   },
   created() {
-    const socket = this.socket = new SocketIoClient('/ns');
+    const socket = this.socket = new SocketIoClient();
     socket.on('echo', data => {
       const time = data.time;
       const now = getNanoSeconds();
@@ -65,15 +101,51 @@ export default {
       this.totalDelta += data.delta;
       this.count ++;
     });
-    socket.on('hello', () => {
-      for (let i = 1; i < 2000; i += 100) {
+    const p2p = this.p2p = new SocketP2p(socket, {}, () => {
+      this.addP2pStatusList('ready');
+    });
+    p2p.on('ping', data => {
+      p2p.emit('echo', data);
+      console.log('receive ping, send echo');
+    });
+    p2p.on('echo', data => {
+      console.log('receive echo.')
+      const time = data.time;
+      const now = getNanoSeconds();
+      data.now = now;
+      data.size = data.text.length;
+      data.delta = now - time;
+      this.p2pEchoList.unshift(data);
+
+      this.p2pTotalDelta += data.delta;
+      this.p2pCount ++;
+    });
+
+    p2p.on('upgrade', data => {
+      this.addP2pStatusList('upgrade');
+    });
+    p2p.on('peer-error', data => {
+      this.addP2pStatusList('peer-error');
+    });
+  },
+  methods: {
+    addP2pStatusList(status) {
+      this.p2pStatusList.push(status);
+    },
+    repeatPing(repeat = 2000) {
+      for (let i = 1; i < repeat; i += 100) {
         setTimeout(() => {
           this.sendPing();
         }, i + 500);
       }
-    });
-  },
-  methods: {
+    },
+    repeatPingP2p(repeat = 2000) {
+      for (let i = 1; i < repeat; i += 100) {
+        setTimeout(() => {
+          this.sendPingP2p();
+        }, i + 500);
+      }
+    },
     sendPing(size = 1024) {
       let text = '';
       while (size--) {
@@ -83,12 +155,24 @@ export default {
         time: getNanoSeconds(),
         text: text,
       }
-      this.socket.emit('ppp', data);
+      this.socket.emit('ping', data);
       console.log('send');
+    },
+    sendPingP2p(size = 1024) {
+      let text = '';
+      while (size--) {
+        text += '1';
+      }
+      const data = {
+        time: getNanoSeconds(),
+        text: text,
+      }
+      this.p2p.emit('ping', data);
+      console.log('send ping');
     },
     fix(value, fix = 3) {
       return Number(value).toFixed(fix);
-    }
+    },
   },
 };
 </script>
